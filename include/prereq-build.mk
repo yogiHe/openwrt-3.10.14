ab13cd40979ae7319f46bb9d6402f9d3c976beab
heyingjun03 <heyingjun03@meituan.com>
2018-07-25 03:43:04 -0700

    change for root limit

--- include/prereq-build.mk
+++ include/prereq-build.mk
@@ -13,7 +13,7 @@ include $(INCLUDE_DIR)/host-build.mk
 PKG_NAME:=Build dependency
 
 define Require/non-root
-	[ "$$(shell whoami)" != "root" ]
+#	[ "$$(shell whoami)" != "root" ]
 endef
 $(eval $(call Require,non-root, \
 	Please do not compile as root. \
